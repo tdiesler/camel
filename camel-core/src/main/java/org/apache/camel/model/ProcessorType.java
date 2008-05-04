a8196bab03d6c33ea02ac100670a3f550768bda0
svk <svk@fd562ee2-2b8a-43b5-a0c1-2240559bccdc>
2008-05-04 05:01:00 +0000

    Merged revisions 41003 via svnmerge from https://projects.open.iona.com/projects/svn/iona/mirrors/apache.org/activemq/camel/trunk
    ........
      r41003 | svk | 2008-05-03 23:59:31 -0500 (Sat, 03 May 2008) | 3 lines
      
       r71883@hades (orig r653179):  ningjiang | 2008-05-03 23:58:47 -0500
       Fixed the CS errors
    ........
    
    
    git-svn-id: http://fusesource.com/forge/svn/fuseeip/trunk@480 fd562ee2-2b8a-43b5-a0c1-2240559bccdc

--- camel-core/src/main/java/org/apache/camel/model/ProcessorType.java
+++ camel-core/src/main/java/org/apache/camel/model/ProcessorType.java
@@ -1094,10 +1094,10 @@ public abstract class ProcessorType<Type extends ProcessorType> implements Block
     public Type setBody(Expression expression) {
         return process(ProcessorBuilder.setBody(expression));
     }
-   
+
     /**
      * Adds a processor which sets the body on the OUT message
-     * 
+     *
      * @deprecated Please use {@link #transform(Expression)} instead
      */
     public Type setOutBody(Expression expression) {
@@ -1112,7 +1112,7 @@ public abstract class ProcessorType<Type extends ProcessorType> implements Block
     public ExpressionClause<ProcessorType<Type>> setOutBody() {
         return transform();
     }
-    
+
     /**
      * Adds a processor which sets the body on the OUT message
      */
@@ -1131,11 +1131,11 @@ public abstract class ProcessorType<Type extends ProcessorType> implements Block
         TransformType answer = new TransformType(clause);
         addOutput(answer);
         return clause;
-    }             
-    
+    }
+
     /**
-	 * Adds a processor which sets the body on the FAULT message
-	 */
+     * Adds a processor which sets the body on the FAULT message
+     */
     public Type setFaultBody(Expression expression) {
         return process(ProcessorBuilder.setFaultBody(expression));
     }
