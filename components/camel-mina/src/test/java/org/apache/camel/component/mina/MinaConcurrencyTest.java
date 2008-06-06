3c01e4a00e67904f5c4619764b018c6e26d64450
njiang <njiang@fd562ee2-2b8a-43b5-a0c1-2240559bccdc>
2008-06-06 06:42:58 +0000

    Change to use DefaultProducerTemplate
    git-svn-id: http://fusesource.com/forge/svn/fuseeip/trunk@636 fd562ee2-2b8a-43b5-a0c1-2240559bccdc

--- components/camel-mina/src/test/java/org/apache/camel/component/mina/MinaConcurrencyTest.java
+++ components/camel-mina/src/test/java/org/apache/camel/component/mina/MinaConcurrencyTest.java
@@ -22,14 +22,16 @@ import org.apache.camel.ContextTestSupport;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.Processor;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.impl.CamelTemplate;
+import org.apache.camel.impl.DefaultProducerTemplate;
 
 public class MinaConcurrencyTest extends ContextTestSupport {
-	
+
     protected String uri = "mina:tcp://localhost:9123";
-    protected CopyOnWriteArraySet<Long> threadSet = new CopyOnWriteArraySet<Long>();  
+    protected CopyOnWriteArraySet<Long> threadSet = new CopyOnWriteArraySet<Long>();
 
     @Override
     protected void setUp() throws Exception {
@@ -38,59 +40,59 @@ public class MinaConcurrencyTest extends ContextTestSupport {
 
     @Override
     protected void tearDown() throws Exception {
-    	super.tearDown();
+        super.tearDown();
     }
 
     public void testConcurrentInOnly() throws Exception {
-    	int count = 10;   	
+        int count = 10;
         MockEndpoint endpoint = getMockEndpoint("mock:result");
         endpoint.expectedMessageCount(2 * count);
         threadSet.clear();
 
         Thread[] threads = new Thread[count];
         for (int i = 0; i < count; i++) {
-        	threads[i] = createSenderThread();
-        	threads[i].start();
+            threads[i] = createSenderThread();
+            threads[i].start();
         }
         for (int i = 0; i < count; i++) {
-        	threads[i].join();
+            threads[i].join();
         }
         assertTrue("Clients should not wait for processing", endpoint.getReceivedCounter() < count);
-        
+
         Thread.sleep(1000);
         for (int i = 0; i < count; i++) {
-        	threads[i] = createSenderThread();
-        	threads[i].start();
+            threads[i] = createSenderThread();
+            threads[i].start();
         }
         for (int i = 0; i < count; i++) {
-        	threads[i].join();
+            threads[i].join();
         }
-        
+
         assertMockEndpointsSatisifed();
         if (Runtime.getRuntime().availableProcessors() > 1) {
-        	// only expect this on a multiple core system
-        	assertTrue("Expected processing in more than one thread", threadSet.size() > 1);
+            // only expect this on a multiple core system
+            assertTrue("Expected processing in more than one thread", threadSet.size() > 1);
         }
     }
-    
+
     protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             public void configure() {
                 from(uri).process(new Processor() {
                     public void process(Exchange exchange) throws Exception {
-                    	// some very slooow processing
-                    	Thread.sleep(1000);
-                    	threadSet.add(Thread.currentThread().getId());
+                        // some very slooow processing
+                        Thread.sleep(1000);
+                        threadSet.add(Thread.currentThread().getId());
                     }
                 }).to("mock:result");
             }
         };
     }
-    
-    protected Thread createSenderThread () {
-    	return new Thread("Sender") {
+
+    protected Thread createSenderThread() {
+        return new Thread("Sender") {
             public void run() {
-            	CamelTemplate<Exchange> t = new CamelTemplate<Exchange>(context);
+                ProducerTemplate<Exchange> t = new DefaultProducerTemplate<Exchange>(context);
                 t.sendBody(uri, ExchangePattern.InOnly, "Hello World!");
             }
         };
