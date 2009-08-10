5a32fa910b03314d58ebc354b8c82794f845afd1
davsclaus <davsclaus@fd562ee2-2b8a-43b5-a0c1-2240559bccdc>
2009-08-10 12:22:50 +0000

    MANAGEMENT: Added a simple camel unit test so we know it works and can use that as inspiration for writing unit tests.
    git-svn-id: http://fusesource.com/forge/svn/fuseeip/trunk@14624 fd562ee2-2b8a-43b5-a0c1-2240559bccdc

---
+++ components/fuse-management/src/test/java/org/fusesource/camel/management/SimpleCamelRouteTest.java
@@ -0,0 +1,34 @@
+package org.fusesource.camel.management;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+/**
+ * Just a simple Camel unit test to see that it works in this component.
+ *
+ * @version $Revision$
+ */
+public class SimpleCamelRouteTest extends CamelTestSupport {
+
+    @Test
+    public void testSimpleRoute() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedBodiesReceived("Hello World");
+
+        template.sendBody("direct:start", "Hello World");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start").to("log:foo").to("mock:result");
+            }
+        };
+    }
+}
