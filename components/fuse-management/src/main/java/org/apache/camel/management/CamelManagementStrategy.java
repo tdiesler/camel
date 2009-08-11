c59b5f1a9294322b2748a2ad50035454c932d408
davsclaus <davsclaus@fd562ee2-2b8a-43b5-a0c1-2240559bccdc>
2009-08-11 13:35:22 +0000

    MANAGEMENT: registering Camel endpoints using fuse commons management.
    git-svn-id: http://fusesource.com/forge/svn/fuseeip/trunk@14659 fd562ee2-2b8a-43b5-a0c1-2240559bccdc

--- components/fuse-management/src/main/java/org/apache/camel/management/CamelManagementStrategy.java
+++ components/fuse-management/src/main/java/org/apache/camel/management/CamelManagementStrategy.java
@@ -1,31 +1,48 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/*
+ * © 2001-2009, Progress Software Corporation and/or its subsidiaries or affiliates.  All rights reserved.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.apache.camel.management;
 
+import javax.management.JMException;
+import javax.management.NotCompliantMBeanException;
 import javax.management.ObjectName;
+import javax.management.modelmbean.InvalidTargetObjectTypeException;
+import javax.management.modelmbean.ModelMBeanInfo;
+import javax.management.modelmbean.RequiredModelMBean;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.fusesource.commons.management.basic.AbstractManagementStrategy;
+import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource;
+import org.springframework.jmx.export.assembler.MetadataMBeanInfoAssembler;
 
 /**
  * @version $Revision$
  */
 public class CamelManagementStrategy extends AbstractManagementStrategy {
 
-    private CamelNamingStrategy naming = new CamelNamingStrategy();
+    private static final Log LOG = LogFactory.getLog(CamelManagementStrategy.class);
+
+    private final CamelNamingStrategy naming = new CamelNamingStrategy();
+    private final MetadataMBeanInfoAssembler assembler;
+
+    public CamelManagementStrategy() {
+        assembler = new MetadataMBeanInfoAssembler();
+        assembler.setAttributeSource(new AnnotationJmxAttributeSource());
+    }
 
     public void addManagedObject(Object managedObject) throws Exception {
         ObjectName objectName = null;
@@ -33,6 +50,11 @@ public class CamelManagementStrategy extends AbstractManagementStrategy {
         if (managedObject instanceof ManagedEndpoint) {
             ManagedEndpoint me = (ManagedEndpoint) managedObject;
             objectName = naming.getObjectName(me);
+        }
+
+        if (managedObject instanceof ManagedRoute) {
+            ManagedRoute mr = (ManagedRoute) managedObject;
+            objectName = naming.getObjectName(mr);
         }
 
         if (objectName != null) {
@@ -44,7 +66,27 @@ public class CamelManagementStrategy extends AbstractManagementStrategy {
     }
 
     private void registerMBean(Object managedObject, ObjectName objectName) throws Exception {
-        super.getMbeanServer().registerMBean(managedObject, objectName);
+        try {
+            super.getMbeanServer().registerMBean(managedObject, objectName);
+        } catch (NotCompliantMBeanException e) {
+            // If this is not a "normal" MBean, then try to deploy it using JMX annotations
+            // TODO: Consider porting this to commons management so everyone easily can register mbeans without
+            // all the JMX hazzle as you just use spring annotations on your managed object
+            // the code is from Camel 1.x developed by Willem Tam from the Sonic Team
+            ModelMBeanInfo mbi = assembler.getMBeanInfo(managedObject, objectName.toString());
+            RequiredModelMBean mbean = (RequiredModelMBean) getMbeanServer().instantiate(RequiredModelMBean.class.getName());
+            mbean.setModelMBeanInfo(mbi);
+            try {
+                mbean.setManagedResource(managedObject, "ObjectReference");
+            } catch (InvalidTargetObjectTypeException itote) {
+                throw new JMException(itote.getMessage());
+            }
+            super.getMbeanServer().registerMBean(mbean, objectName);
+        }
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Registered MBean: " + managedObject + " with name: " + objectName);
+        }
     }
 
 
--- components/fuse-management/src/main/java/org/apache/camel/management/CommonManagementLifecycleStrategy.java
+++ components/fuse-management/src/main/java/org/apache/camel/management/CommonManagementLifecycleStrategy.java
@@ -1,18 +1,18 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/*
+ * © 2001-2009, Progress Software Corporation and/or its subsidiaries or affiliates.  All rights reserved.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.apache.camel.management;
 
@@ -41,7 +41,8 @@ public class CommonManagementLifecycleStrategy implements LifecycleStrategy {
     public void onEndpointAdd(Endpoint endpoint) {
         endpointCounter++;
         try {
-            strategy.addManagedObject(endpoint);
+            ManagedEndpoint me = new ManagedEndpoint(endpoint);
+            strategy.addManagedObject(me);
         } catch (Exception e) {
             throw ObjectHelper.wrapRuntimeCamelException(e);
         }
@@ -54,9 +55,16 @@ public class CommonManagementLifecycleStrategy implements LifecycleStrategy {
     }
 
     public void onRoutesAdd(Collection<Route> routes) {
+        for (Route route : routes) {
+
+        }
     }
 
     public int getEndpointCounter() {
         return endpointCounter;
+    }
+
+    public CamelManagementStrategy getStrategy() {
+        return strategy;
     }
 }
