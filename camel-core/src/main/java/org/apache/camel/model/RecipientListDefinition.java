04be6faf9eeb49a7a8c6fd341bd40075d993f948
davsclaus <claus.ibsen@gmail.com>
2011-05-10 18:30:25 +0200

    Fixed camel model getTimeout returning null instead of 0 if timeout has not been set.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1101481 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    
    	camel-core/src/main/java/org/apache/camel/model/MulticastDefinition.java
    	camel-core/src/main/java/org/apache/camel/model/SplitDefinition.java

--- camel-core/src/main/java/org/apache/camel/model/RecipientListDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/RecipientListDefinition.java
@@ -110,7 +110,8 @@ public class RecipientListDefinition<Type extends ProcessorDefinition> extends N
             executorService = routeContext.getCamelContext().getExecutorServiceStrategy().newDefaultThreadPool(this, "RecipientList");
         }
         answer.setExecutorService(executorService);
-        if (getTimeout() > 0 && !isParallelProcessing()) {
+        long timeout = getTimeout() != null ? getTimeout() : 0;
+        if (timeout > 0 && !isParallelProcessing()) {
             throw new IllegalArgumentException("Timeout is used but ParallelProcessing has not been enabled.");
         }
 
@@ -321,7 +322,7 @@ public class RecipientListDefinition<Type extends ProcessorDefinition> extends N
     }
 
     public Long getTimeout() {
-        return timeout != null ? timeout : 0;
+        return timeout;
     }
 
     public void setTimeout(Long timeout) {
