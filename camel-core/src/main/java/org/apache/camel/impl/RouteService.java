34ef3830ff1623a12522ccfef7de916cafefff88
Claus Ibsen <davsclaus@apache.org>
2011-06-11 08:13:59 +0000

    CAMEL-4044: Polished
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1134549 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/impl/RouteService.java
+++ camel-core/src/main/java/org/apache/camel/impl/RouteService.java
@@ -244,7 +244,7 @@ public class RouteService extends ServiceSupport {
 
     protected void startChildService(Route route, List<Service> services) throws Exception {
         for (Service service : services) {
-            LOG.debug("Starting {}", service);
+            LOG.debug("Starting child service on route: {} -> {}", route.getId(), service);
             for (LifecycleStrategy strategy : camelContext.getLifecycleStrategies()) {
                 strategy.onServiceAdd(camelContext, service, route);
             }
@@ -255,6 +255,7 @@ public class RouteService extends ServiceSupport {
 
     protected void stopChildService(Route route, Set<Service> services, boolean shutdown) throws Exception {
         for (Service service : services) {
+            LOG.debug("Stopping child service on route: {} -> {}", route.getId(), service);
             for (LifecycleStrategy strategy : camelContext.getLifecycleStrategies()) {
                 strategy.onServiceRemove(camelContext, service, route);
             }
