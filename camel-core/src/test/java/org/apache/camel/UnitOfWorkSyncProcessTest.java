07864bbb8f08c79cb7c655b7094cab3a232a3c52
davsclaus <claus.ibsen@gmail.com>
2011-11-06 14:34:55 +0100

    CAMEL-4010: FTP consumer should process UoW synchronously to ensure done work is done with same thread, as the FTP libraries is not thread safe.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1198340 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    
    	components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/FtpOperations.java

--- camel-core/src/test/java/org/apache/camel/UnitOfWorkSyncProcessTest.java
+++ camel-core/src/test/java/org/apache/camel/UnitOfWorkSyncProcessTest.java
@@ -22,7 +22,7 @@ import java.util.concurrent.Executors;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultEndpoint;
 import org.apache.camel.impl.DefaultExchange;
-import org.apache.camel.support.SynchronizationAdapter;
+import org.apache.camel.impl.SynchronizationAdapter;
 
 /**
  *
