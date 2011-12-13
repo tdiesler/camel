14502b54456297c9d4f3a4f17a4f5ea47510079e
Willem Ning Jiang <ningjiang@apache.org>
2011-12-13 07:11:22 +0000

    Merged revisions 1213531 via svnmerge from https://svn.apache.org/repos/asf/camel/branches/camel-2.8.x
    ................
      r1213531 | ningjiang | 2011-12-13 09:11:54 +0800 (Tue, 13 Dec 2011) | 13 lines
    
      Merged revisions 1213219,1213526 via svnmerge from
      https://svn.apache.org/repos/asf/camel/trunk
    
      ........
        r1213219 | ningjiang | 2011-12-12 20:56:31 +0800 (Mon, 12 Dec 2011) | 1 line
    
        CAMEL-4768 Fix the issue that camel-http4 component send the endpoint parameters
      ........
        r1213526 | ningjiang | 2011-12-13 08:58:49 +0800 (Tue, 13 Dec 2011) | 1 line
    
        CAMEL-4768 fix a CS error
      ........
    ................
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.7.x@1213579 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-http4/src/main/java/org/apache/camel/component/http4/HttpComponent.java
+++ components/camel-http4/src/main/java/org/apache/camel/component/http4/HttpComponent.java
@@ -177,7 +177,10 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
         // create the configurer to use for this endpoint
         HttpClientConfigurer configurer = createHttpClientConfigurer(parameters);
         URI endpointUri = URISupport.createRemainingURI(new URI(addressUri), CastUtils.cast(httpClientParameters));
-        // restructure uri to be based on the parameters left as we dont want to include the Camel internal options
+        // create the endpoint and set the http uri to be null
+        HttpEndpoint endpoint = new HttpEndpoint(endpointUri.toString(), this, clientParams, clientConnectionManager, configurer);
+        // configure the endpoint
+        setProperties(endpoint, parameters);
         // The httpUri should be start with http or https
         String httpUriAddress = addressUri;
         if (addressUri.startsWith("http4")) {
@@ -186,6 +189,8 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
         if (addressUri.startsWith("https4")) {
             httpUriAddress = "https" + addressUri.substring(6);
         }
+        // restructure uri to be based on the parameters left as we dont want to include the Camel internal options
+        // build up the http uri
         URI httpUri = URISupport.createRemainingURI(new URI(httpUriAddress), CastUtils.cast(parameters));
 
         // validate http uri that end-user did not duplicate the http part that can be a common error
@@ -197,15 +202,7 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
                         "The uri part is not configured correctly. You have duplicated the http(s) protocol.");
             }
         }
-
-        // register port on schema registry
-        boolean secure = isSecureConnection(uri);
-        int port = getPort(httpUri);
-        registerPort(secure, x509HostnameVerifier, port);
-        
-        // create the endpoint
-        HttpEndpoint endpoint = new HttpEndpoint(endpointUri.toString(), this, httpUri, clientParams, clientConnectionManager, configurer);
-        setProperties(endpoint, parameters);
+        endpoint.setHttpUri(httpUri);
         setEndpointHeaderFilterStrategy(endpoint);
         endpoint.setBinding(getHttpBinding());
         if (httpBinding != null) {
@@ -214,10 +211,14 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
         if (httpClientConfigurer != null) {
             endpoint.setHttpClientConfigurer(httpClientConfigurer);
         }
+        // register port on schema registry
+        boolean secure = isSecureConnection(uri);
+        int port = getPort(httpUri);
+        registerPort(secure, x509HostnameVerifier, port);
 
         return endpoint;
     }
-
+   
     private static int getPort(URI uri) {
         int port = uri.getPort();
         if (port < 0) {
