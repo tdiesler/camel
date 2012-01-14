c9396b06c07e3e613394ef2dfa1d04f4e19d08fd
davsclaus <claus.ibsen@gmail.com>
2012-01-14 14:46:08 +0100

    CAMEL-4870: Attachments is not propagated in the template components such as camel-stringtemplate
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1228603 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    
    	components/camel-stringtemplate/src/main/java/org/apache/camel/component/stringtemplate/StringTemplateEndpoint.java
    	components/camel-velocity/src/main/java/org/apache/camel/component/velocity/VelocityEndpoint.java

--- components/camel-velocity/src/main/java/org/apache/camel/component/velocity/VelocityEndpoint.java
+++ components/camel-velocity/src/main/java/org/apache/camel/component/velocity/VelocityEndpoint.java
@@ -22,7 +22,6 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.apache.camel.Exchange;
@@ -145,7 +144,6 @@ public class VelocityEndpoint extends ResourceBasedEndpoint {
         return (VelocityEndpoint) getCamelContext().getEndpoint(newUri);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     protected void onExchange(Exchange exchange) throws Exception {
         String path = getResourceUri();
@@ -185,7 +183,7 @@ public class VelocityEndpoint extends ResourceBasedEndpoint {
         // getResourceAsInputStream also considers the content cache
         StringWriter buffer = new StringWriter();
         String logTag = getClass().getName();
-        Map variableMap = ExchangeHelper.createVariableMap(exchange);
+        Map<String, Object> variableMap = ExchangeHelper.createVariableMap(exchange);
         Context velocityContext = new VelocityContext(variableMap);
 
         // let velocity parse and generate the result in buffer
@@ -196,11 +194,7 @@ public class VelocityEndpoint extends ResourceBasedEndpoint {
         // now lets output the results to the exchange
         Message out = exchange.getOut();
         out.setBody(buffer.toString());
-
-        Map<String, Object> headers = (Map<String, Object>) velocityContext.get("headers");
-        for (Entry<String, Object> entry : headers.entrySet()) {
-            out.setHeader(entry.getKey(), entry.getValue());
-        }
+        out.setHeaders(exchange.getIn().getHeaders());
+        out.setAttachments(exchange.getIn().getAttachments());
     }
-
 }
