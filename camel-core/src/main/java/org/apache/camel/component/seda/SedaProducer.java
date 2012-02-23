3d6e6dba8e5c8dc8076589370e8d479cba49c034
davsclaus <claus.ibsen@gmail.com>
2012-02-23 11:19:56 +0100

    CAMEL-5033: Seda producer in synced mode should not handover on completion tasks.

--- camel-core/src/main/java/org/apache/camel/component/seda/SedaProducer.java
+++ camel-core/src/main/java/org/apache/camel/component/seda/SedaProducer.java
@@ -44,12 +44,6 @@ public class SedaProducer extends CollectionProducer {
 
     @Override
     public boolean process(final Exchange exchange, final AsyncCallback callback) {
-        // use a new copy of the exchange to route async and handover the on completion to the new copy
-        // so its the new copy that performs the on completion callback when its done
-        Exchange copy = ExchangeHelper.createCorrelatedCopy(exchange, true);
-        // set a new from endpoint to be the seda queue
-        copy.setFromEndpoint(endpoint);
-
         WaitForTaskToComplete wait = waitForTaskToComplete;
         if (exchange.getProperty(Exchange.ASYNC_WAIT) != null) {
             wait = exchange.getProperty(Exchange.ASYNC_WAIT, WaitForTaskToComplete.class);
@@ -57,6 +51,9 @@ public class SedaProducer extends CollectionProducer {
 
         if (wait == WaitForTaskToComplete.Always
             || (wait == WaitForTaskToComplete.IfReplyExpected && ExchangeHelper.isOutCapable(exchange))) {
+
+            // do not handover the completion as we wait for the copy to complete, and copy its result back when it done
+            Exchange copy = prepareCopy(exchange, false);
 
             // latch that waits until we are complete
             final CountDownLatch latch = new CountDownLatch(1);
@@ -131,6 +128,8 @@ public class SedaProducer extends CollectionProducer {
             }
         } else {
             // no wait, eg its a InOnly then just add to queue and return
+            // handover the completion so its the copy which performs that, as we do not wait
+            Exchange copy = prepareCopy(exchange, true);
             if (log.isTraceEnabled()) {
                 log.trace("Adding Exchange to queue: " + copy);
             }
@@ -141,6 +140,14 @@ public class SedaProducer extends CollectionProducer {
         // so we should just signal the callback we are done synchronously
         callback.done(true);
         return true;
+    }
+
+    protected Exchange prepareCopy(Exchange exchange, boolean handover) {
+        // use a new copy of the exchange to route async
+        Exchange copy = ExchangeHelper.createCorrelatedCopy(exchange, handover);
+        // set a new from endpoint to be the seda queue
+        copy.setFromEndpoint(endpoint);
+        return copy;
     }
 
     @Override
