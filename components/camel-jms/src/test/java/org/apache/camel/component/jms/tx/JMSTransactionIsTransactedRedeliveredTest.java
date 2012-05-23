87f4c58d683620b3db859843d63f5a77eeb65f11
Claus Ibsen <davsclaus@apache.org>
2012-05-23 13:17:13 +0000

    Fixed test on a slower box
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1341854 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jms/src/test/java/org/apache/camel/component/jms/tx/JMSTransactionIsTransactedRedeliveredTest.java
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/tx/JMSTransactionIsTransactedRedeliveredTest.java
@@ -78,6 +78,8 @@ public class JMSTransactionIsTransactedRedeliveredTest extends CamelSpringTestSu
         error.assertIsSatisfied();
 
         // check JMX stats
+        // need a little sleep to ensure JMX is updated
+        Thread.sleep(500);
 
         ObjectName name = ObjectName.getInstance("org.apache.camel:context=localhost/camel-1,type=routes,name=\"myRoute\"");
 
