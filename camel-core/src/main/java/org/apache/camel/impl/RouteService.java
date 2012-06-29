85a01b64ef854f9ec7989d23d5de3b330b0b8f07
Claus Ibsen <davsclaus@apache.org>
2012-06-29 11:41:41 +0000

    CAMEL-5401: Fixed route policy ref not working with all callbacks being invoked.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1355326 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/impl/RouteService.java
+++ camel-core/src/main/java/org/apache/camel/impl/RouteService.java
@@ -171,8 +171,8 @@ public class RouteService extends ChildServiceSupport {
             ServiceHelper.startService(route);
 
             // invoke callbacks on route policy
-            if (routeDefinition.getRoutePolicies() != null) {
-                for (RoutePolicy routePolicy : routeDefinition.getRoutePolicies()) {
+            if (route.getRouteContext().getRoutePolicyList() != null) {
+                for (RoutePolicy routePolicy : route.getRouteContext().getRoutePolicyList()) {
                     routePolicy.onStart(route);
                 }
             }
@@ -220,8 +220,8 @@ public class RouteService extends ChildServiceSupport {
             }
 
             // invoke callbacks on route policy
-            if (routeDefinition.getRoutePolicies() != null) {
-                for (RoutePolicy routePolicy : routeDefinition.getRoutePolicies()) {
+            if (route.getRouteContext().getRoutePolicyList() != null) {
+                for (RoutePolicy routePolicy : route.getRouteContext().getRoutePolicyList()) {
                     routePolicy.onStop(route);
                 }
             }
@@ -262,8 +262,8 @@ public class RouteService extends ChildServiceSupport {
             // see more details in the warmUp method
             ServiceHelper.stopAndShutdownServices(route.getEndpoint());
             // invoke callbacks on route policy
-            if (routeDefinition.getRoutePolicies() != null) {
-                for (RoutePolicy routePolicy : routeDefinition.getRoutePolicies()) {
+            if (route.getRouteContext().getRoutePolicyList() != null) {
+                for (RoutePolicy routePolicy : route.getRouteContext().getRoutePolicyList()) {
                     routePolicy.onRemove(route);
                 }
             }
@@ -293,8 +293,8 @@ public class RouteService extends ChildServiceSupport {
         // suspend and resume logic is provided by DefaultCamelContext which leverages ShutdownStrategy
         // to safely suspend and resume
         for (Route route : routes) {
-            if (routeDefinition.getRoutePolicies() != null) {
-                for (RoutePolicy routePolicy : routeDefinition.getRoutePolicies()) {
+            if (route.getRouteContext().getRoutePolicyList() != null) {
+                for (RoutePolicy routePolicy : route.getRouteContext().getRoutePolicyList()) {
                     routePolicy.onSuspend(route);
                 }
             }
@@ -306,8 +306,8 @@ public class RouteService extends ChildServiceSupport {
         // suspend and resume logic is provided by DefaultCamelContext which leverages ShutdownStrategy
         // to safely suspend and resume
         for (Route route : routes) {
-            if (routeDefinition.getRoutePolicies() != null) {
-                for (RoutePolicy routePolicy : routeDefinition.getRoutePolicies()) {
+            if (route.getRouteContext().getRoutePolicyList() != null) {
+                for (RoutePolicy routePolicy : route.getRouteContext().getRoutePolicyList()) {
                     routePolicy.onResume(route);
                 }
             }
