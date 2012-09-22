tdiesler/camel

commit cee856fecb8777c82a61edeb258ea4213f1b0042

CAMEL-5632: Fixed issue with loading resource containg relative paths. Fixed issue with 2+ parent paths using .. notation.
git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1388762 13f79535-47bb-0310-9956-ffa450edef68

camel-core/src/main/java/org/apache/camel/util/FileUtil.java
@@ -202,7 +202,8 @@ public final class FileUtil {
     }
 
     /**
-     * Compacts a path by stacking it and reducing <tt>..</tt>
+     * Compacts a path by stacking it and reducing <tt>..</tt>,
+     * and uses OS specific file separators (eg {@link java.io.File#separator}).
      */
     public static String compactPath(String path) {
         if (path == null) {
@@ -219,6 +220,9 @@ public final class FileUtil {
             return path;
         }
 
+        // preserve ending slash if given in input path
+        boolean endsWithSlash = path.endsWith("/") || path.endsWith("\\");
+
         Stack<String> stack = new Stack<String>();
         
         String separatorRegex = File.separator;
@@ -227,8 +231,8 @@ public final class FileUtil {
         }
         String[] parts = path.split(separatorRegex);
         for (String part : parts) {
-            if (part.equals("..") && !stack.isEmpty()) {
-                // only pop if there is a previous path
+            if (part.equals("..") && !stack.isEmpty() && !"..".equals(stack.peek())) {
+                // only pop if there is a previous path, which is not a ".." path either
                 stack.pop();
             } else {
                 stack.push(part);
@@ -242,6 +246,10 @@ public final class FileUtil {
             if (it.hasNext()) {
                 sb.append(File.separator);
             }
+        }
+
+        if (endsWithSlash) {
+            sb.append(File.separator);
         }
 
         return sb.toString();

camel-core/src/test/java/org/apache/camel/util/FileUtilTest.java
@@ -133,6 +133,10 @@ public class FileUtilTest extends TestCase {
     public void testCompactPath() {
         assertEquals(null, FileUtil.compactPath(null));
         if (FileUtil.isWindows()) {
+            assertEquals("..\\foo", FileUtil.compactPath("..\\foo"));
+            assertEquals("..\\..\\foo", FileUtil.compactPath("..\\..\\foo"));
+            assertEquals("..\\..\\foo\\bar", FileUtil.compactPath("..\\..\\foo\\bar"));
+            assertEquals("..\\..\\foo", FileUtil.compactPath("..\\..\\foo\\bar\\.."));
             assertEquals("foo", FileUtil.compactPath("foo"));
             assertEquals("bar", FileUtil.compactPath("foo\\..\\bar"));
             assertEquals("bar\\baz", FileUtil.compactPath("foo\\..\\bar\\baz"));
@@ -141,6 +145,10 @@ public class FileUtilTest extends TestCase {
             assertEquals("..\\baz", FileUtil.compactPath("foo\\bar\\..\\..\\..\\baz"));
             assertEquals("..\\foo\\bar", FileUtil.compactPath("..\\foo\\bar"));
         } else {
+            assertEquals("../foo", FileUtil.compactPath("../foo"));
+            assertEquals("../../foo", FileUtil.compactPath("../../foo"));
+            assertEquals("../../foo/bar", FileUtil.compactPath("../../foo/bar"));
+            assertEquals("../../foo", FileUtil.compactPath("../../foo/bar/.."));
             assertEquals("foo", FileUtil.compactPath("foo"));
             assertEquals("bar", FileUtil.compactPath("foo/../bar"));
             assertEquals("bar/baz", FileUtil.compactPath("foo/../bar/baz"));

camel-core/src/main/java/org/apache/camel/util/ObjectHelper.java
@@ -905,17 +905,7 @@ public final class ObjectHelper {
      * @return the modified or unmodified string if there were no changes
      */
     private static String resolveUriPath(String name) {
-        String answer = name;
-        if (answer.indexOf("//") > -1) {
-            answer = answer.replaceAll("//", "/");
-        }
-        if (answer.indexOf("../") > -1) {
-            answer = answer.replaceAll("[A-Za-z0-9]*/\\.\\./", "");
-        }
-        if (answer.indexOf("./") > -1) {
-            answer = answer.replaceAll("\\./", "");
-        }
-        return answer;
+        return FileUtil.compactPath(name);
     }
 
     /**

commit 184c8ca433522304048bdfb58d0c7fc5dea70dbf

fixed a regression in ValidatorSchemaImportTest which was introduced with r1388762
git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1388906 13f79535-47bb-0310-9956-ffa450edef68

camel-core/src/main/java/org/apache/camel/util/FileUtil.java
@@ -209,12 +209,7 @@ public final class FileUtil {
         if (path == null) {
             return null;
         }
-
-        // only normalize path if it contains .. as we want to avoid: path/../sub/../sub2 as this can leads to trouble
-        if (path.indexOf("..") == -1) {
-            return path;
-        }
-
+        
         // only normalize if contains a path separator
         if (path.indexOf(File.separator) == -1) {
             return path;
@@ -234,6 +229,8 @@ public final class FileUtil {
             if (part.equals("..") && !stack.isEmpty() && !"..".equals(stack.peek())) {
                 // only pop if there is a previous path, which is not a ".." path either
                 stack.pop();
+            } else if (part.equals(".") || part.isEmpty()) {
+                // do nothing because we don't want a path like foo/./bar
             } else {
                 stack.push(part);
             }
@@ -379,6 +376,7 @@ public final class FileUtil {
         return renamed;
     }
 
+    @SuppressWarnings("resource")
     public static void copyFile(File from, File to) throws IOException {
         FileChannel in = new FileInputStream(from).getChannel();
         FileChannel out = new FileOutputStream(to).getChannel();

camel-core/src/test/java/org/apache/camel/util/FileUtilTest.java
@@ -144,6 +144,8 @@ public class FileUtilTest extends TestCase {
             assertEquals("baz", FileUtil.compactPath("foo\\bar\\..\\..\\baz"));
             assertEquals("..\\baz", FileUtil.compactPath("foo\\bar\\..\\..\\..\\baz"));
             assertEquals("..\\foo\\bar", FileUtil.compactPath("..\\foo\\bar"));
+            assertEquals("foo\\bar\\baz", FileUtil.compactPath("foo\\bar\\.\\baz"));
+            assertEquals("foo\\bar\\baz", FileUtil.compactPath("foo\\bar\\\\baz"));
         } else {
             assertEquals("../foo", FileUtil.compactPath("../foo"));
             assertEquals("../../foo", FileUtil.compactPath("../../foo"));
@@ -156,6 +158,8 @@ public class FileUtilTest extends TestCase {
             assertEquals("baz", FileUtil.compactPath("foo/bar/../../baz"));
             assertEquals("../baz", FileUtil.compactPath("foo/bar/../../../baz"));
             assertEquals("../foo/bar", FileUtil.compactPath("../foo/bar"));
+            assertEquals("foo/bar/baz", FileUtil.compactPath("foo/bar/./baz"));
+            assertEquals("foo/bar/baz", FileUtil.compactPath("foo/bar//baz"));
         }
     }
 