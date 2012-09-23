84ad3f49ac605673b29afbec572fd58d5cb5e428
davsclaus <claus.ibsen@gmail.com>
2012-09-23 10:33:21 +0200

    Merged revisions 1388790 via  git cherry-pick from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r1388790 | dkulp | 2012-09-22 08:39:58 -0400 (Sat, 22 Sep 2012) | 2 lines
    
      Some optimizations for writing the stream caches out to another stream to avoid some buffer copies
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1388791 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    
    	components/camel-cxf/src/main/java/org/apache/camel/component/cxf/interceptors/RawMessageContentRedirectInterceptor.java

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/interceptors/RawMessageContentRedirectInterceptor.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/interceptors/RawMessageContentRedirectInterceptor.java
@@ -20,6 +20,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
+import org.apache.camel.StreamCache;
 import org.apache.camel.util.IOHelper;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
@@ -47,9 +48,12 @@ public class RawMessageContentRedirectInterceptor extends AbstractPhaseIntercept
         if (null != params) {
             InputStream is = (InputStream)params.get(0);
             OutputStream os = message.getContent(OutputStream.class);
-
             try {
-                IOHelper.copy(is, os);
+                if (is instanceof StreamCache) {
+                    ((StreamCache)is).writeTo(os);
+                } else {
+                    IOUtils.copy(is, os);
+                }
             } catch (Exception e) {
                 throw new Fault(e);
             } finally {
