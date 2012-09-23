30850a0b04463fa8eed9ddf4f6bde214d95a8226
davsclaus <claus.ibsen@gmail.com>
2012-09-23 10:41:38 +0200

    Merged revisions 1388217 via  git cherry-pick from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r1388217 | dkulp | 2012-09-20 17:06:56 -0400 (Thu, 20 Sep 2012) | 2 lines
    
      Use CXF's IOUtils instead of Camels as CXF's will have some CXF specific optimizations for this use case.
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1388218 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    
    	components/camel-cxf/src/main/java/org/apache/camel/component/cxf/interceptors/RawMessageContentRedirectInterceptor.java

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/interceptors/RawMessageContentRedirectInterceptor.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/interceptors/RawMessageContentRedirectInterceptor.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 import org.apache.camel.StreamCache;
 import org.apache.camel.util.IOHelper;
+import org.apache.cxf.helpers.IOUtils;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
