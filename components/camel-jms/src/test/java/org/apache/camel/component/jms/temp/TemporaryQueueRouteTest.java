5d97e90a2ff5971a79973687c4186dc58a04d920
davsclaus <claus.ibsen@gmail.com>
2013-03-15 09:40:55 +0100

    Disabled test that may fail on CI

--- components/camel-jms/src/test/java/org/apache/camel/component/jms/temp/TemporaryQueueRouteTest.java
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/temp/TemporaryQueueRouteTest.java
@@ -23,6 +23,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.CamelJmsTestHelper;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.apache.camel.component.jms.JmsComponent.jmsComponentAutoAcknowledge;
@@ -30,6 +31,7 @@ import static org.apache.camel.component.jms.JmsComponent.jmsComponentAutoAcknow
 /**
  * @version 
  */
+@Ignore
 public class TemporaryQueueRouteTest extends CamelTestSupport {
     protected String endpointUri = "activemq:temp:queue:cheese";
 
