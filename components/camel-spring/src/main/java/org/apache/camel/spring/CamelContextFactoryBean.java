0903b19b8266e3b216dde5d4a67076f9ac559abe
Claus Ibsen <davsclaus@apache.org>
2014-02-28 14:54:00 +0100

    CAMEL-7156: SpringCamelContext should shutdown eager to shutdown cleanly. Introduced shutdownEager option to turn back old behavior for special use-case relying on old way.
    Conflicts:
    	components/camel-spring/src/main/java/org/apache/camel/spring/SpringCamelContext.java

--- components/camel-spring/src/main/java/org/apache/camel/spring/CamelContextFactoryBean.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/CamelContextFactoryBean.java
@@ -54,6 +54,7 @@ import org.apache.camel.model.dataformat.DataFormatsDefinition;
 import org.apache.camel.spi.PackageScanFilter;
 import org.apache.camel.spi.Registry;
 import org.apache.camel.spring.spi.BridgePropertyPlaceholderConfigurer;
+import org.apache.camel.util.CamelContextHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
@@ -97,6 +98,8 @@ public class CamelContextFactoryBean extends AbstractCamelContextFactoryBean<Spr
     private String errorHandlerRef;
     @XmlAttribute(required = false)
     private String autoStartup;
+    @XmlAttribute(required = false)
+    private String shutdownEager;
     @XmlAttribute(required = false)
     private String useMDCLogging;
     @XmlAttribute(required = false)
@@ -224,6 +227,17 @@ public class CamelContextFactoryBean extends AbstractCamelContextFactoryBean<Spr
         if (beanPostProcessor != null) {
             // Inject the annotated resource
             beanPostProcessor.postProcessBeforeInitialization(builder, builder.toString());
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        super.afterPropertiesSet();
+
+        Boolean shutdownEager = CamelContextHelper.parseBoolean(getContext(), getShutdownEager());
+        if (shutdownEager != null) {
+            LOG.debug("Using shutdownEager: " + shutdownEager);
+            getContext().setShutdownEager(shutdownEager);
         }
     }
 
@@ -493,6 +507,14 @@ public class CamelContextFactoryBean extends AbstractCamelContextFactoryBean<Spr
 
     public void setAutoStartup(String autoStartup) {
         this.autoStartup = autoStartup;
+    }
+
+    public String getShutdownEager() {
+        return shutdownEager;
+    }
+
+    public void setShutdownEager(String shutdownEager) {
+        this.shutdownEager = shutdownEager;
     }
 
     public String getUseMDCLogging() {
