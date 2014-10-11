7a048bab4b5c9cc8cd09f41c73268306ccf0eccb
Willem Jiang <willem.jiang@gmail.com>
2014-10-11 15:04:05 +0800

    CAMEL-7888 Fix the issue that HL7Decoder leaks memory
    Conflicts:
    	components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL7Converter.java

--- components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL7Converter.java
+++ components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL7Converter.java
@@ -16,11 +16,13 @@
  */
 package org.apache.camel.component.hl7;
 
+import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.parser.Parser;
 import ca.uhn.hl7v2.parser.PipeParser;
-
+import ca.uhn.hl7v2.validation.impl.ValidationContextFactory;
 import org.apache.camel.Converter;
 
 /**
@@ -28,7 +30,9 @@ import org.apache.camel.Converter;
  */
 @Converter
 public final class HL7Converter {
-
+    
+    private static final HapiContext DEFAULT_CONTEXT = new DefaultHapiContext(ValidationContextFactory.noValidation());
+    
     private HL7Converter() {
         // Helper class
     }
@@ -39,10 +43,20 @@ public final class HL7Converter {
     }
 
     @Converter
+    public static byte[] toByteArray(Message message) throws HL7Exception {
+        return message.encode().getBytes();
+    }
+
+    @Converter
     public static Message toMessage(String body) throws HL7Exception {
         return parse(body, new PipeParser());
     }
-    
+
+    @Converter
+    public static Message toMessage(byte[] body) throws HL7Exception {
+        return parse(new String(body), DEFAULT_CONTEXT.getGenericParser());
+    }
+
     static Message parse(String body, Parser parser) throws HL7Exception {
         return parser.parse(body);
     }    
