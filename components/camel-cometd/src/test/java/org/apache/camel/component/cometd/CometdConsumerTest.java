880cfc789aec79abe076a887759af04b37ec2afa
Andrea Cosentino <ancosen@gmail.com>
2016-04-21 14:42:25 +0200

    [ENTESB-5007] CAMEL-8637: Upgrade Jetty version to 9.x in Camel-CometD - Fixed CS

--- components/camel-cometd/src/test/java/org/apache/camel/component/cometd/CometdConsumerTest.java
+++ components/camel-cometd/src/test/java/org/apache/camel/component/cometd/CometdConsumerTest.java
@@ -40,7 +40,6 @@ import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
