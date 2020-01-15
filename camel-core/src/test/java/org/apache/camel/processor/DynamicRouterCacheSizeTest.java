856c4ce5ee0b6e0315b0aa5116b1933c8a8ab58c
John Poth <poth.john@gmail.com>
2020-01-15 17:41:24 +0100

    [ENTESB-11575][PATCH-2975] Add test for ServicePoolAware Producers

--- camel-core/src/test/java/org/apache/camel/processor/DynamicRouterCacheSizeTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/DynamicRouterCacheSizeTest.java
@@ -16,12 +16,29 @@
  */
 package org.apache.camel.processor;
 
+import java.util.Map;
+import org.apache.camel.AsyncCallback;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
 import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Producer;
+import org.apache.camel.ServicePoolAware;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockComponent;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultAsyncProducer;
 import org.junit.Test;
 
 public class DynamicRouterCacheSizeTest extends ContextTestSupport {
 
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext camelContext = super.createCamelContext();
+        camelContext.addComponent("service-pool-aware", new ServicePoolAwareComponent());
+        return camelContext;
+    }
 
     @Test
     public void testCache() {
@@ -35,6 +52,18 @@ public class DynamicRouterCacheSizeTest extends ContextTestSupport {
         assertEquals(0, proc.producerCache.size());
     }
 
+    @Test
+    public void testServicePoolAwareCache() {
+        template.sendBodyAndHeader("direct:pool", "Hello World", "whereto", "foo");
+        template.sendBodyAndHeader("direct:pool", "Bye World", "whereto", "foo");
+        template.sendBodyAndHeader("direct:pool", "Hi World", "whereto", "bar");
+
+
+        SendDynamicProcessor proc = (SendDynamicProcessor) context.getProcessor("myServicePoolAwareCacheSize");
+        assertEquals(0, proc.producerCache.getCapacity());
+        assertEquals(0, proc.producerCache.size());
+    }
+
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
@@ -42,7 +71,44 @@ public class DynamicRouterCacheSizeTest extends ContextTestSupport {
             public void configure() throws Exception {
                 from("direct:start")
                         .toD("mock:${header.whereto}", -1).id("myCacheSize");
+
+                from("direct:pool")
+                        .toD("service-pool-aware:${header.whereto}", -1).id("myServicePoolAwareCacheSize");
             }
         };
+    }
+
+    private class ServicePoolAwareComponent extends MockComponent {
+        @Override
+        protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) {
+            SericePoolAwareEndpoint endpoint = new SericePoolAwareEndpoint(uri, this);
+            endpoint.setName(remaining);
+            return endpoint;
+        }
+    }
+
+    private class SericePoolAwareEndpoint extends MockEndpoint {
+        public SericePoolAwareEndpoint(String endpointUri, Component component) {
+            super(endpointUri, component);
+        }
+        @Override
+        public Producer createProducer() {
+            return new ServicePoolAwareProducer(this) {
+                public boolean process(Exchange exchange, AsyncCallback callback) {
+                    onExchange(exchange);
+                    callback.done(true);
+                    return true;
+                }
+            };
+        }
+    }
+
+    private class ServicePoolAwareProducer extends DefaultAsyncProducer implements ServicePoolAware {
+        public ServicePoolAwareProducer(Endpoint endpoint) {
+            super(endpoint);
+        }
+        public boolean process(Exchange exchange, AsyncCallback callback) {
+            return false;
+        }
     }
 }
