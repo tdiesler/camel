6a1f32be6ae48a7004e4c2006afc94628f48be0c
Tom Cunningham <tcunning@redhat.com>
2021-09-17 18:28:42 -0400

    Add generated catalog files and check in deserializer changes for camel-master inclusion

--- dsl/camel-yaml-dsl/camel-yaml-dsl-deserializers/src/generated/java/org/apache/camel/dsl/yaml/deserializers/EndpointConsumerDeserializersResolver.java
+++ dsl/camel-yaml-dsl/camel-yaml-dsl-deserializers/src/generated/java/org/apache/camel/dsl/yaml/deserializers/EndpointConsumerDeserializersResolver.java
@@ -16,6 +16,19 @@ public final class EndpointConsumerDeserializersResolver implements YamlDeserial
     public static String resolveEndpointUri(String id, Node node) {
         switch(id) {
             case "activemq":
+            case "ahc-ws":
+            case "ahc-wss":
+            case "amqp":
+            case "as2":
+            case "asterisk":
+            case "atmos":
+            case "atmosphere-websocket":
+            case "atom":
+            case "atomix-map":
+            case "atomix-messaging":
+            case "atomix-queue":
+            case "atomix-set":
+            case "atomix-value":
             case "avro":
             case "aws2-ddbstream":
             case "aws2-kinesis":
@@ -26,7 +39,20 @@ public final class EndpointConsumerDeserializersResolver implements YamlDeserial
             case "azure-storage-blob":
             case "azure-storage-datalake":
             case "azure-storage-queue":
+            case "beanstalk":
+            case "box":
             case "browse":
+            case "cmis":
+            case "coap":
+            case "coap+tcp":
+            case "coaps":
+            case "coaps+tcp":
+            case "cometd":
+            case "cometds":
+            case "consul":
+            case "corda":
+            case "couchbase":
+            case "couchdb":
             case "cql":
             case "cron":
             case "cxf":
@@ -38,9 +64,23 @@ public final class EndpointConsumerDeserializersResolver implements YamlDeserial
             case "debezium-sqlserver":
             case "direct":
             case "direct-vm":
+            case "disruptor":
+            case "disruptor-vm":
+            case "docker":
+            case "dropbox":
+            case "ehcache":
+            case "elsql":
+            case "etcd-stats":
+            case "etcd-watch":
+            case "facebook":
+            case "fhir":
             case "file":
+            case "file-watch":
+            case "flatpack":
             case "ftp":
             case "ftps":
+            case "git":
+            case "github":
             case "google-calendar":
             case "google-calendar-stream":
             case "google-drive":
@@ -50,13 +90,44 @@ public final class EndpointConsumerDeserializersResolver implements YamlDeserial
             case "google-sheets":
             case "google-sheets-stream":
             case "google-storage":
+            case "gora":
+            case "grpc":
+            case "guava-eventbus":
+            case "hazelcast-instance":
+            case "hazelcast-list":
+            case "hazelcast-map":
+            case "hazelcast-multimap":
+            case "hazelcast-queue":
+            case "hazelcast-replicatedmap":
+            case "hazelcast-seda":
+            case "hazelcast-set":
+            case "hazelcast-topic":
+            case "hbase":
+            case "hdfs":
+            case "iec60870-client":
+            case "iec60870-server":
+            case "ignite-cache":
+            case "ignite-events":
+            case "ignite-messaging":
             case "imap":
             case "imaps":
+            case "infinispan":
             case "infinispan-embedded":
+            case "irc":
+            case "ironmq":
             case "jbpm":
+            case "jcache":
+            case "jclouds":
+            case "jcr":
             case "jetty":
+            case "jgroups":
+            case "jgroups-raft":
             case "jira":
             case "jms":
+            case "jmx":
+            case "jooq":
+            case "jpa":
+            case "jt400":
             case "kafka":
             case "kamelet":
             case "kamelet-reify":
@@ -69,33 +140,76 @@ public final class EndpointConsumerDeserializersResolver implements YamlDeserial
             case "kubernetes-pods":
             case "kubernetes-replication-controllers":
             case "kubernetes-services":
+            case "lumberjack":
+            case "master":
+            case "milo-client":
+            case "milo-server":
+            case "mina":
+            case "minio":
             case "mllp":
             case "mongodb":
             case "mongodb-gridfs":
+            case "mybatis":
+            case "nats":
             case "netty":
             case "netty-http":
+            case "nitrite":
+            case "nsq":
+            case "oaipmh":
+            case "olingo2":
+            case "olingo4":
+            case "optaplanner":
+            case "paho":
+            case "paho-mqtt5":
+            case "pg-replication-slot":
+            case "pgevent":
             case "platform-http":
             case "pop3":
             case "pop3s":
+            case "pubnub":
+            case "pulsar":
             case "quartz":
+            case "quickfix":
+            case "rabbitmq":
+            case "reactive-streams":
             case "ref":
             case "rest":
             case "rest-api":
+            case "resteasy":
+            case "robotframework":
+            case "rss":
             case "salesforce":
             case "scheduler":
             case "seda":
+            case "service":
+            case "servlet":
             case "sftp":
+            case "sip":
+            case "sips":
             case "sjms":
+            case "sjms2":
             case "slack":
+            case "smpp":
+            case "smpps":
             case "smtp":
             case "smtps":
+            case "snmp":
+            case "soroush":
+            case "splunk":
             case "spring-event":
+            case "spring-integration":
+            case "spring-rabbitmq":
             case "spring-redis":
+            case "spring-ws":
             case "sql":
+            case "ssh":
+            case "stomp":
             case "stream":
             case "stub":
             case "telegram":
+            case "thrift":
             case "timer":
+            case "twilio":
             case "twitter-directmessage":
             case "twitter-search":
             case "twitter-timeline":
@@ -104,8 +218,18 @@ public final class EndpointConsumerDeserializersResolver implements YamlDeserial
             case "vertx-kafka":
             case "vertx-websocket":
             case "vm":
+            case "weather":
+            case "web3j":
             case "webhook":
+            case "websocket":
+            case "websocket-jsr356":
+            case "wordpress":
+            case "xmpp":
             case "xquery":
+            case "yammer":
+            case "zendesk":
+            case "zookeeper":
+            case "zookeeper-master":
             return org.apache.camel.dsl.yaml.common.YamlSupport.creteEndpointUri(id, node);
         }
         return null;
--- dsl/camel-yaml-dsl/camel-yaml-dsl-deserializers/src/generated/java/org/apache/camel/dsl/yaml/deserializers/EndpointProducerDeserializersResolver.java
+++ dsl/camel-yaml-dsl/camel-yaml-dsl-deserializers/src/generated/java/org/apache/camel/dsl/yaml/deserializers/EndpointProducerDeserializersResolver.java
@@ -16,6 +16,22 @@ public final class EndpointProducerDeserializersResolver implements YamlDeserial
     public static String resolveEndpointUri(String id, Node node) {
         switch(id) {
             case "activemq":
+            case "ahc":
+            case "ahc-ws":
+            case "ahc-wss":
+            case "amqp":
+            case "arangodb":
+            case "as2":
+            case "asterisk":
+            case "atlasmap":
+            case "atmos":
+            case "atmosphere-websocket":
+            case "atomix-map":
+            case "atomix-messaging":
+            case "atomix-multimap":
+            case "atomix-queue":
+            case "atomix-set":
+            case "atomix-value":
             case "avro":
             case "aws-secrets-manager":
             case "aws2-athena":
@@ -44,21 +60,67 @@ public final class EndpointProducerDeserializersResolver implements YamlDeserial
             case "azure-storage-datalake":
             case "azure-storage-queue":
             case "bean":
+            case "bean-validator":
+            case "beanstalk":
+            case "bonita":
+            case "box":
+            case "braintree":
             case "browse":
+            case "caffeine-cache":
+            case "caffeine-loadcache":
+            case "chatscript":
+            case "chunk":
             case "class":
+            case "cm-sms":
+            case "cmis":
+            case "coap":
+            case "coap+tcp":
+            case "coaps":
+            case "coaps+tcp":
+            case "cometd":
+            case "cometds":
+            case "consul":
             case "controlbus":
+            case "corda":
+            case "couchbase":
+            case "couchdb":
             case "cql":
+            case "crypto":
             case "cxf":
             case "cxfrs":
             case "dataformat":
             case "dataset":
             case "dataset-test":
+            case "digitalocean":
             case "direct":
             case "direct-vm":
+            case "disruptor":
+            case "disruptor-vm":
+            case "djl":
+            case "dns":
+            case "docker":
+            case "dozer":
+            case "drill":
+            case "dropbox":
+            case "ehcache":
             case "elasticsearch-rest":
+            case "elsql":
+            case "etcd-keys":
+            case "etcd-stats":
+            case "exec":
+            case "facebook":
+            case "fhir":
             case "file":
+            case "flatpack":
+            case "flink":
+            case "fop":
+            case "freemarker":
             case "ftp":
             case "ftps":
+            case "ganglia":
+            case "geocoder":
+            case "git":
+            case "github":
             case "google-bigquery":
             case "google-bigquery-sql":
             case "google-calendar":
@@ -68,19 +130,62 @@ public final class EndpointProducerDeserializersResolver implements YamlDeserial
             case "google-pubsub":
             case "google-sheets":
             case "google-storage":
+            case "gora":
             case "grape":
+            case "graphql":
+            case "grpc":
+            case "guava-eventbus":
+            case "hazelcast-atomicvalue":
+            case "hazelcast-list":
+            case "hazelcast-map":
+            case "hazelcast-multimap":
+            case "hazelcast-queue":
+            case "hazelcast-replicatedmap":
+            case "hazelcast-ringbuffer":
+            case "hazelcast-seda":
+            case "hazelcast-set":
+            case "hazelcast-topic":
+            case "hbase":
+            case "hdfs":
             case "http":
             case "https":
             case "hwcloud-functiongraph":
             case "hwcloud-iam":
             case "hwcloud-smn":
+            case "iec60870-client":
+            case "iec60870-server":
+            case "ignite-cache":
+            case "ignite-compute":
+            case "ignite-idgen":
+            case "ignite-messaging":
+            case "ignite-queue":
+            case "ignite-set":
             case "imap":
             case "imaps":
+            case "infinispan":
             case "infinispan-embedded":
+            case "influxdb":
+            case "iota":
+            case "ipfs":
+            case "irc":
+            case "ironmq":
             case "jbpm":
+            case "jcache":
+            case "jclouds":
+            case "jcr":
             case "jdbc":
+            case "jgroups":
+            case "jgroups-raft":
+            case "jing":
             case "jira":
             case "jms":
+            case "jolt":
+            case "jooq":
+            case "jpa":
+            case "jslt":
+            case "json-validator":
+            case "jsonata":
+            case "jt400":
             case "kafka":
             case "kamelet":
             case "kamelet-reify":
@@ -99,39 +204,110 @@ public final class EndpointProducerDeserializersResolver implements YamlDeserial
             case "kubernetes-secrets":
             case "kubernetes-service-accounts":
             case "kubernetes-services":
+            case "kudu":
             case "language":
+            case "ldap":
+            case "ldif":
             case "log":
+            case "lpr":
+            case "lucene":
+            case "metrics":
+            case "micrometer":
             case "microprofile-metrics":
+            case "milo-client":
+            case "milo-server":
+            case "mina":
+            case "minio":
             case "mllp":
             case "mock":
             case "mongodb":
             case "mongodb-gridfs":
+            case "msv":
+            case "mustache":
+            case "mvel":
+            case "mybatis":
+            case "mybatis-bean":
+            case "nagios":
+            case "nats":
             case "netty":
             case "netty-http":
+            case "nitrite":
+            case "nsq":
+            case "oaipmh":
+            case "olingo2":
+            case "olingo4":
             case "openshift-build-configs":
             case "openshift-builds":
+            case "openstack-cinder":
+            case "openstack-glance":
+            case "openstack-keystone":
+            case "openstack-neutron":
+            case "openstack-nova":
+            case "openstack-swift":
+            case "optaplanner":
+            case "paho":
+            case "paho-mqtt5":
+            case "pdf":
+            case "pgevent":
             case "pop3":
             case "pop3s":
+            case "pubnub":
+            case "pulsar":
+            case "quickfix":
+            case "rabbitmq":
+            case "reactive-streams":
             case "ref":
             case "rest":
             case "rest-openapi":
             case "rest-swagger":
+            case "resteasy":
+            case "robotframework":
             case "saga":
             case "salesforce":
+            case "sap-netweaver":
+            case "schematron":
+            case "scp":
             case "seda":
             case "servicenow":
             case "sftp":
+            case "sip":
+            case "sips":
             case "sjms":
+            case "sjms2":
             case "slack":
+            case "smpp":
+            case "smpps":
             case "smtp":
             case "smtps":
+            case "snmp":
+            case "solr":
+            case "solrCloud":
+            case "solrs":
+            case "soroush":
+            case "spark":
+            case "splunk":
+            case "splunk-hec":
+            case "spring-batch":
             case "spring-event":
+            case "spring-integration":
+            case "spring-jdbc":
+            case "spring-ldap":
+            case "spring-rabbitmq":
             case "spring-redis":
+            case "spring-ws":
             case "sql":
             case "sql-stored":
+            case "ssh":
+            case "stax":
+            case "stitch":
+            case "stomp":
             case "stream":
+            case "string-template":
             case "stub":
             case "telegram":
+            case "thrift":
+            case "tika":
+            case "twilio":
             case "twitter-directmessage":
             case "twitter-search":
             case "twitter-timeline":
@@ -143,9 +319,24 @@ public final class EndpointProducerDeserializersResolver implements YamlDeserial
             case "vertx-kafka":
             case "vertx-websocket":
             case "vm":
+            case "weather":
+            case "web3j":
+            case "websocket":
+            case "websocket-jsr356":
+            case "weka":
+            case "wordpress":
+            case "workday":
+            case "xchange":
+            case "xj":
+            case "xmlsecurity-sign":
+            case "xmlsecurity-verify":
+            case "xmpp":
             case "xquery":
             case "xslt":
             case "xslt-saxon":
+            case "yammer":
+            case "zendesk":
+            case "zookeeper":
             return org.apache.camel.dsl.yaml.common.YamlSupport.creteEndpointUri(id, node);
         }
         return null;
