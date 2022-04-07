588f8277e81e59ce9b573d3ba3118afeffa68f8c
Radovan Netuka <rnetuka@localhost.localdomain>
2022-04-07 09:46:17 +0200

    [ENTESB-18702] Performance improvements on Camel File component

--- camel-core/src/main/java/org/apache/camel/component/file/FileConsumer.java
+++ camel-core/src/main/java/org/apache/camel/component/file/FileConsumer.java
@@ -20,13 +20,15 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
@@ -57,10 +59,8 @@ public class FileConsumer extends GenericFileConsumer<File> {
     }
 
     @Override
-    protected boolean pollDirectory(String fileName, List<GenericFile<File>> fileList, int depth) {
+    protected boolean pollDirectory(String fileName, List<GenericFile<File>> fileList, final int depth) {
         log.trace("pollDirectory from fileName: {}", fileName);
-
-        depth++;
 
         File directory = new File(fileName);
         if (!directory.exists() || !directory.isDirectory()) {
@@ -72,107 +72,120 @@ public class FileConsumer extends GenericFileConsumer<File> {
         }
 
         log.trace("Polling directory: {}", directory.getPath());
-        File[] dirFiles = directory.listFiles();
-        if (dirFiles == null || dirFiles.length == 0) {
-            // no files in this directory to poll
-            if (log.isTraceEnabled()) {
-                log.trace("No files found in directory: {}", directory.getPath());
-            }
-            return true;
-        } else {
-            // we found some files
-            if (log.isTraceEnabled()) {
-                log.trace("Found {} in directory: {}", dirFiles.length, directory.getPath());
-            }
-        }
-        List<File> files = Arrays.asList(dirFiles);
-        if (getEndpoint().isPreSort()) {
-            Collections.sort(files, (a, b) -> a.getAbsoluteFile().compareTo(a.getAbsoluteFile()));
-        }
 
-        for (File file : dirFiles) {
-            // check if we can continue polling in files
-            if (!canPollMoreFiles(fileList)) {
+        try (Stream<Path> dirStream = list(directory.toPath())) {
+
+            if (isDirectoryEmpty(directory.toPath())) {
+                // no files in this directory to poll
+                if (log.isTraceEnabled()) {
+                    log.trace("No files found in directory: {}", directory.getPath());
+                }
+                return true;
+            } else {
+                // we found some files
+                if (log.isTraceEnabled()) {
+                    log.trace("Found files in directory: {}", directory.getPath());
+                }
+            }
+
+            final CanPollMore canPollMore = new CanPollMore();
+            if (!canPollMore.check(canPollMoreFiles(fileList))) {
                 return false;
             }
 
-            // trace log as Windows/Unix can have different views what the file is?
-            if (log.isTraceEnabled()) {
-                log.trace("Found file: {} [isAbsolute: {}, isDirectory: {}, isFile: {}, isHidden: {}]",
-                        new Object[]{file, file.isAbsolute(), file.isDirectory(), file.isFile(), file.isHidden()});
-            }
+            dirStream.forEach(file -> {
+                // check if we can continue polling in files
+                if (canPollMore.check(canPollMoreFiles(fileList))) {
 
-            // creates a generic file
-            GenericFile<File> gf = asGenericFile(endpointPath, file, getEndpoint().getCharset(), getEndpoint().isProbeContentType());
-
-            if (file.isDirectory()) {
-                if (endpoint.isRecursive() && depth < endpoint.getMaxDepth() && isValidFile(gf, true, files)) {
-                    // recursive scan and add the sub files and folders
-                    String subDirectory = fileName + File.separator + file.getName();
-                    boolean canPollMore = pollDirectory(subDirectory, fileList, depth);
-                    if (!canPollMore) {
-                        return false;
+                    // trace log as Windows/Unix can have different views what the file is?
+                    if (log.isTraceEnabled()) {
+                        log.trace("Found file: {} [isAbsolute: {}, isDirectory: {}, isFile: {}, isHidden: {}]",
+                                new Object[]{file, file.isAbsolute(), Files.isDirectory(file), Files.isRegularFile(file), file.toFile().isHidden()});
                     }
-                }
-            } else {
-                // Windows can report false to a file on a share so regard it always as a file (if its not a directory)
-                if (depth >= endpoint.minDepth && isValidFile(gf, false, files)) {
-                    log.trace("Adding valid file: {}", file);
-                    // matched file so add
-                    if (extendedAttributes != null) {
-                        Path path = file.toPath();
-                        Map<String, Object> allAttributes = new HashMap<>();
-                        for (String attribute : extendedAttributes) {
-                            try {
-                                String prefix = null;
-                                if (attribute.endsWith(":*")) {
-                                    prefix = attribute.substring(0, attribute.length() - 1);
-                                } else if (attribute.equals("*")) {
-                                    prefix = "basic:";
-                                }
 
-                                if (ObjectHelper.isNotEmpty(prefix)) {
-                                    Map<String, Object> attributes = Files.readAttributes(path, attribute);
-                                    if (attributes != null) {
-                                        for (Map.Entry<String, Object> entry : attributes.entrySet()) {
-                                            allAttributes.put(prefix + entry.getKey(), entry.getValue());
+                    // creates a generic file
+                    GenericFile<File> gf = asGenericFile(endpointPath, file.toFile(), getEndpoint().getCharset(), getEndpoint().isProbeContentType());
+
+                    if (Files.isDirectory(file)) {
+                        if (endpoint.isRecursive() && depth + 1 < endpoint.getMaxDepth() && isValidFile(gf, true, Collections.emptyList())) {
+                            // recursive scan and add the sub files and folders
+                            String subDirectory = fileName + File.separator + file.getFileName();
+                            canPollMore.check(pollDirectory(subDirectory, fileList, depth + 1));
+                        }
+                    } else {
+                        // Windows can report false to a file on a share so regard it always as a file (if its not a directory)
+                        if (depth + 1 >= endpoint.minDepth && isValidFile(gf, false, Collections.emptyList())) {
+                            log.trace("Adding valid file: {}", file);
+                            // matched file so add
+                            if (extendedAttributes != null) {
+                                Path path = file;
+                                Map<String, Object> allAttributes = new HashMap<>();
+                                for (String attribute : extendedAttributes) {
+                                    try {
+                                        String prefix = null;
+                                        if (attribute.endsWith(":*")) {
+                                            prefix = attribute.substring(0, attribute.length() - 1);
+                                        } else if (attribute.equals("*")) {
+                                            prefix = "basic:";
+                                        }
+
+                                        if (ObjectHelper.isNotEmpty(prefix)) {
+                                            Map<String, Object> attributes = Files.readAttributes(path, attribute);
+                                            if (attributes != null) {
+                                                for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+                                                    allAttributes.put(prefix + entry.getKey(), entry.getValue());
+                                                }
+                                            }
+                                        } else if (!attribute.contains(":")) {
+                                            allAttributes.put("basic:" + attribute, Files.getAttribute(path, attribute));
+                                        } else {
+                                            allAttributes.put(attribute, Files.getAttribute(path, attribute));
+                                        }
+                                    } catch (IOException e) {
+                                        if (log.isDebugEnabled()) {
+                                            log.debug("Unable to read attribute {} on file {}", attribute, file, e);
                                         }
                                     }
-                                } else if (!attribute.contains(":")) {
-                                    allAttributes.put("basic:" + attribute, Files.getAttribute(path, attribute));
-                                } else {
-                                    allAttributes.put(attribute, Files.getAttribute(path, attribute));
                                 }
-                            } catch (IOException e) {
-                                if (log.isDebugEnabled()) {
-                                    log.debug("Unable to read attribute {} on file {}", attribute, file, e);
-                                }
+
+                                gf.setExtendedAttributes(allAttributes);
                             }
+
+                            fileList.add(gf);
                         }
-
-                        gf.setExtendedAttributes(allAttributes);
                     }
-
-                    fileList.add(gf);
                 }
-
-            }
+            });
+            return canPollMore.value;
+        } catch (IOException ex) {
+            throw new GenericFileOperationFailedException("IOException while listing files in: " + directory, ex);
         }
-
-        return true;
     }
 
     @Override
     protected boolean isMatched(GenericFile<File> file, String doneFileName, List<File> files) {
         String onlyName = FileUtil.stripPath(doneFileName);
+        Path parentDirectory = file.getFile().toPath().getParent();
+        Path doneFile = Paths.get(parentDirectory.toString(), onlyName);
         // the done file name must be among the files
-        for (File f : files) {
-            if (f.getName().equals(onlyName)) {
-                return true;
-            }
+        if (Files.exists(doneFile)) {
+            return true;
+        } else {
+            log.trace("Done file: {} does not exist", doneFileName);
+            return false;
         }
-        log.trace("Done file: {} does not exist", doneFileName);
-        return false;
+    }
+
+    private Stream<Path> list(Path directory) throws IOException {
+        return getEndpoint().isPreSort()
+                ? Files.list(directory).sorted(Comparator.comparing(Path::toAbsolutePath))
+                : Files.list(directory);
+    }
+
+    private boolean isDirectoryEmpty(Path directory) throws IOException {
+        try (Stream<Path> entries = Files.list(directory)) {
+            return !entries.findFirst().isPresent();
+        }
     }
 
     /**
@@ -205,8 +218,12 @@ public class FileConsumer extends GenericFileConsumer<File> {
         answer.setEndpointPath(endpointPath);
         answer.setFile(file);
         answer.setFileNameOnly(file.getName());
-        answer.setFileLength(file.length());
-        answer.setDirectory(file.isDirectory());
+        try {
+            answer.setFileLength(Files.size(file.toPath()));
+        } catch (IOException ex) {
+            answer.setFileLength(file.length());
+        }
+        answer.setDirectory(Files.isDirectory(file.toPath()));
         // must use FileUtil.isAbsolute to have consistent check for whether the file is
         // absolute or not. As windows do not consider \ paths as absolute where as all
         // other OS platforms will consider \ as absolute. The logic in Camel mandates
@@ -214,7 +231,11 @@ public class FileConsumer extends GenericFileConsumer<File> {
         // to return a consistent answer for all OS platforms.
         answer.setAbsolute(FileUtil.isAbsolute(file));
         answer.setAbsoluteFilePath(file.getAbsolutePath());
-        answer.setLastModified(file.lastModified());
+        try {
+            answer.setLastModified(Files.getLastModifiedTime(file.toPath()).toMillis());
+        } catch (IOException ex) {
+            answer.setLastModified(file.lastModified());
+        }
 
         // compute the file path as relative to the starting directory
         File path;
@@ -267,4 +288,16 @@ public class FileConsumer extends GenericFileConsumer<File> {
         // GenericFile's absolute path is always up to date whereas the underlying file is not
         return !file.getFile().getAbsolutePath().equals(file.getAbsoluteFilePath());
     }
+
+    private static class CanPollMore {
+
+        public boolean value = true;
+
+        public boolean check(boolean value) {
+            this.value = this.value && value;
+            return this.value;
+        }
+
+    }
+
 }
