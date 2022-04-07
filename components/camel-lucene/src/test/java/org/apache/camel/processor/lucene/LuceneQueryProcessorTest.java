50866a70f162299333d49c8951f6d861fda80e65
JiriOndrusek <ondrusek.jiri@gmail.com>
2022-04-07 16:14:54 +0200

    Upgrade Lucene to be in sync with Elasticsearch

--- components/camel-lucene/src/test/java/org/apache/camel/processor/lucene/LuceneQueryProcessorTest.java
+++ components/camel-lucene/src/test/java/org/apache/camel/processor/lucene/LuceneQueryProcessorTest.java
@@ -62,7 +62,7 @@ public class LuceneQueryProcessorTest extends CamelTestSupport {
                 try {
                     from("direct:start").
                         setHeader("QUERY", constant("Rodney Dangerfield")).
-                        process(new LuceneQueryProcessor("target/stdindexDir", analyzer, null, 20)).
+                        process(new LuceneQueryProcessor("target/stdindexDir", analyzer, null, 20, 20)).
                         to("direct:next");
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -106,7 +106,7 @@ public class LuceneQueryProcessorTest extends CamelTestSupport {
                 try {
                     from("direct:start").
                         setHeader("QUERY", constant("Carl*")).
-                        process(new LuceneQueryProcessor("target/simpleindexDir", analyzer, null, 20)).
+                        process(new LuceneQueryProcessor("target/simpleindexDir", analyzer, null, 20, 20)).
                         to("direct:next");
                 } catch (Exception e) {
                     e.printStackTrace();
